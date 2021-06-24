188f11013b987cbe2e5bd77973c7b8043836034b
gudwh14 <alxious@naver.com>
2021-06-24 12:46:29 +0900

    feat : Create SecurityConfig 파일, h2-console을 위한 configure 설정

---
+++ back-end/src/main/java/me/jjo/gymbook/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package me.jjo.gymbook.config;
+
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers(
+                        "/h2-console/**"
+                );
+    }
+}

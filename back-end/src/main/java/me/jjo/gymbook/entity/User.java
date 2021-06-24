7141e5207aae8e605691d9fb85d758459c899f60
gudwh14 <alxious@naver.com>
2021-06-24 10:57:24 +0900

    feat : Create User Entity

---
+++ back-end/src/main/java/me/jjo/gymbook/entity/User.java
@@ -0,0 +1,42 @@
+package me.jjo.gymbook.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "password",length = 100 , nullable = false)
+    private String password;
+
+    @JsonIgnore
+    @Column(name = "phone", length = 15, nullable = false, unique = true)
+    private String phone;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+    private Set<Authority> authorities;
+
+    @Builder
+    public User(String password, String phone) {
+        this.password = password;
+        this.phone = phone;
+    }
+}

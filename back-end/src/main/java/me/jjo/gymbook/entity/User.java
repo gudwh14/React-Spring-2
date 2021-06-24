5d4a6293d143aa7f824d75865d760d5a065d1359
gudwh14 <alxious@naver.com>
2021-06-24 11:07:00 +0900

    refactor : referencedColumnName 수정
    @JoinTable ,  joinColumns 의referencedColumnName 을 "user_id" -> "id" 로 수정함
    참조하는 컬럼명이 user_id 가 아닌 id 이기 때문

--- back-end/src/main/java/me/jjo/gymbook/entity/User.java
+++ back-end/src/main/java/me/jjo/gymbook/entity/User.java
@@ -30,7 +30,7 @@ public class User {
     @ManyToMany
     @JoinTable(
             name = "user_authority",
-            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
             inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
     private Set<Authority> authorities;
 

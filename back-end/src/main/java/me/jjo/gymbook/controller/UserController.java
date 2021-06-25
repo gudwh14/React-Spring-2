22316d2eec201a90994c619ed494fd11492e1f48
gudwh14 <alxious@naver.com>
2021-06-25 14:53:00 +0900

    refactor : login api 응답 String -> TokenDTO

--- back-end/src/main/java/me/jjo/gymbook/controller/UserController.java
+++ back-end/src/main/java/me/jjo/gymbook/controller/UserController.java
@@ -10,11 +10,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +23,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("/user")
@@ -59,7 +56,7 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@Valid @RequestBody UserLoginDto userLoginDto) {
+    public ResponseEntity<TokenDto> login(@Valid @RequestBody UserLoginDto userLoginDto) {
         logger.debug("user Info : {}",userLoginDto.toString());
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(userLoginDto.getPhone(), userLoginDto.getPassword());
@@ -74,6 +71,9 @@ public class UserController {
 
         return ResponseEntity.ok()
                 .headers(httpHeaders)
-                .body(jwt);
+                .body(TokenDto
+                        .builder()
+                        .token(jwt)
+                        .build());
     }
 }
